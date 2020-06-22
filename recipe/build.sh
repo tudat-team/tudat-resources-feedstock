7da6370bd8bc49de39b4894e841c73e538d2255d
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-22 17:51:29 +0200

    Initial feedstock commit with conda-smithy 3.7.3.

---
+++ recipe/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+mkdir build
+
+cd build
+
+cmake \
+    -DCMAKE_CXX_STANDARD=14 \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DPREFIX=$PREFIX \
+    ..
+
+make -j2
+
+ctest
+
+make install
