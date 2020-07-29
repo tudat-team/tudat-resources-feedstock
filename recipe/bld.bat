4a4c9e8c61b403cd93a87eb85b2f854fa1fddcfd
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-07-29 23:43:36 +0200

    Attempt at mgw for windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,8 @@ if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 cmake ^
-    -G "Visual Studio 15 2017 Win64" ^
-    -DCMAKE_CXX_STANDARD=17 ^
+    -G "MinGW Makefiles" ^
+    -DCMAKE_CXX_STANDARD=14 ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
