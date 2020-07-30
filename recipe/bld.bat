e6cfdea677559028937c38335d2e099116d1ccab
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-07-30 17:08:39 +0200

    Attempt at mgw for windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,12 +3,10 @@ if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 cmake ^
-    -G "NMake Makefiles" ^
+    -G "MinGW Makefiles" ^
     -DCMAKE_CXX_STANDARD=14 ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    -DCMAKE_C_COMPILER_WORKS=1 ^
-    -DCMAKE_CXX_COMPILER_WORKS=1 ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DPREFIX=%LIBRARY_PREFIX% ^
     ..
