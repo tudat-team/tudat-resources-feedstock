de3e81d1c7ff6bce7aecb735d3f90c34c6fc4213
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-09-06 23:19:19 +0200

    Modified test script

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -2,5 +2,5 @@ set HIDDEN_PATH=%HOMEDRIVE%/%HOMEPATH%/.tudat
 if errorlevel 1 exit 1
 if not exist "%HIDDEN_PATH%" mkdir "%HIDDEN_PATH%"
 if errorlevel 1 exit 1
-move "%CONDA_PREFIX%/resource" "%HIDDEN_PATH%"
+move "%PREFIX%/resource" "%HIDDEN_PATH%"
 if errorlevel 1 exit 1
