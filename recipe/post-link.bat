410d5427f3e4324b5bc9714b63e20fed5aab8aa4
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-09-07 20:05:28 +0200

    Fixed xcopy command in post-link.bat

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -10,5 +10,5 @@ if not exist "%HIDDEN_PATH%/resource" mkdir "%HIDDEN_PATH%/resource"
 if errorlevel 1 exit 1
 
 :: attempt to copy resources to home folder
-xcopy /s %PREFIX%/resource %HIDDEN_PATH%/resource
+xcopy /s "%PREFIX%/resource" "%HIDDEN_PATH%/resource"
 if errorlevel 1 exit 1
