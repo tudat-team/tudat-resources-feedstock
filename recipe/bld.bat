cae0b5ee93f441f1b1a8de64aedb288d2d1288de
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-09-11 12:26:48 +0200

    Update

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,6 @@ cmake ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DPREFIX=%LIBRARY_PREFIX% ^
     ..
 if errorlevel 1 exit 1
 cmake --build . --config RelWithDebInfo --target install
