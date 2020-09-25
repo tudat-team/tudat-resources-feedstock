3303ce69a95ae35d79a86eb60216a08ae33b60f8
ggarrett13 <g.h.garrett13@gmail.com>
2020-09-25 09:00:03 +0200

    Made windows build type Release

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,7 +10,7 @@ cmake ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     ..
 if errorlevel 1 exit 1
-cmake --build . --config RelWithDebInfo --target install
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 ctest
 if errorlevel 1 exit 1
