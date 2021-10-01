8429c4051c39da81a1d91d6761e8adbaabb4c5c0
Geoffrey H. Garrett <g.h.garrett13@gmail.com>
2021-10-01 15:27:19 +0200

    Updated `post-link` scripts and hash

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -1,18 +1,32 @@
-:: set hidden path
+@REM set hidden path for tudat
 set HIDDEN_PATH=%HOMEDRIVE%/%HOMEPATH%/.tudat
+set TEMP_PATH=./tmp/
 
-:: create destination hidden folder
+@REM Set resource git URL and REV
+set RESOURCE_GIT_URL="https://github.com/tudat-team/tudat-resources.git"
+
+@REM Read whole variable of git_rev.txt file into variable
+set RESOURCE_GIT_REV="%GIT_FULL_HASH%"
+
+@REM Create destination hidden folder
 if not exist "%HIDDEN_PATH%" mkdir "%HIDDEN_PATH%"
 if errorlevel 1 exit 1
 
-:: create destination resource folder
+@REM Create destination resource folder
 if not exist "%HIDDEN_PATH%/resource" mkdir "%HIDDEN_PATH%/resource"
 if errorlevel 1 exit 1
 
-:: debug
+@REM debug
 echo "PREFIX: %PREFIX%"
 
-:: attempt to copy resources to home folder
-xcopy "%PREFIX%/Library/resource" "%HIDDEN_PATH%/resource" /s /e /y
+@REM Create temp  folder
+if not exist "%TEMP_PATH%" mkdir "%TEMP_PATH%"
+if errorlevel 1 exit 1
+
+@REM Clone resource git repository
+git clone --depth 1 --branch %RESOURCE_GIT_REV% %RESOURCE_GIT_URL% "%TEMP_PATH%"    
+
+@REM attempt to copy resources to home folder
+xcopy "%TEMP_PATH%/resource" "%HIDDEN_PATH%/resource" /s /e /y
 if errorlevel 1 exit 1
 
--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
+RESOURCE_GIT_URL="https://github.com/tudat-team/tudat-resources.git"
+RESOURCE_GIT_REV=$GIT_FULL_HASH
 
 # set hidden path
 HIDDEN_PATH=$HOME/.tudat
+TARGET_PATH="$HIDDEN_PATH"/resource/
+TEMP_PATH=./tmp/
 
 # create destination hidden folder & resource folder
-mkdir -p "$HIDDEN_PATH/resource/"
+mkdir -p $TARGET_PATH
 
-# debug
-echo "PREFIX PATH: $PREFIX"
+# clone the git repo to a tmp directory
+git clone $RESOURCE_GIT_URL $TEMP_PATH --verbose
 
-# attempt to copy resources to home folder
-cp -a "$PREFIX"/resource/. "$HIDDEN_PATH"/resource/
+# enter the tmp directory
+cd $TEMP_PATH/
 
+# switch git branch to commit 
+git checkout $RESOURCE_GIT_REV
 
+# copy the resource subdirectory to
+cp -a ./resource/. $TARGET_PATH
+
+# go back 2 levels
+cd ../
+
+# delete the tmp directory
+rm -rf $TEMP_PATH
